49ef3b9bb83a445a47824391dddd71224a840e66
Aakash <aakashgaurav456@gmail.com>
2023-04-03 10:30:26 +0530

    Backend

--- google.outh.js
+++ google.outh.js
@@ -12,9 +12,11 @@ const passport = require("passport");
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: "573107347042-ptgrj9v6g5fcoden848gpivrql1h2fe0.apps.googleusercontent.com",
-    clientSecret: "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",
-    callbackURL: "http://localhost:3500/auth/google/callback"
+    /* clientID: "573107347042-ptgrj9v6g5fcoden848gpivrql1h2fe0.apps.googleusercontent.com",
+    clientSecret: "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", */
+    clientID: "891522867506-0mi405hsla1lgban32qjq4aq6lilo20m.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: "https://chatpointbackend2-production.up.railway.app/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, cb) {
     await userModel.insertMany([{name:profile._json.name,email:profile._json.email,password:"123"}]);
